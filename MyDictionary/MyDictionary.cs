9f85cefd5f389ba555e1b7b8059b188c27412347
Volkan <erenvolki@gmail.com>
2021-01-25 18:07:42 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TAnahtar, TDeger>
+    {
+        TAnahtar[] _anahtar;
+        TDeger[] _deger;
+
+        TAnahtar[] geciciAnahtar;
+        TDeger[] geciciDeger;
+
+        public MyDictionary()
+        {
+            _anahtar = new TAnahtar[0];
+            _deger = new TDeger[0];
+        }
+
+        public void Add(TAnahtar kelime1, TDeger kelime2)
+        {
+            geciciAnahtar = _anahtar;
+            geciciDeger = _deger;
+
+            _anahtar = new TAnahtar[_anahtar.Length + 1];
+            _deger = new TDeger[_deger.Length + 1];
+
+            for (int i = 0; i < geciciAnahtar.Length; i++)
+            {
+                _anahtar[i] = geciciAnahtar[i];
+            }
+
+            for (int i = 0; i < geciciDeger.Length; i++)
+            {
+                _deger[i] = geciciDeger[i];
+            }
+
+            _anahtar[_anahtar.Length - 1] = kelime1;
+            _deger[_deger.Length - 1] = kelime2;
+        }
+
+        public TAnahtar[] anahtar
+        {
+            get { return _anahtar; }
+        }
+
+        public TDeger[] deger
+        {
+            get { return _deger; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> kelimeler = new MyDictionary<string, string>();
+            kelimeler.Add("Bilgisayar", "Computer");
+            kelimeler.Add("Ürün", "Product");
+            kelimeler.Add("Maaş", "Salary");
+            kelimeler.Add("İşçi", "Worker");
+
+            Console.WriteLine("Basit bir Türkçe İngilizce sözlük uygulaması");
+            Console.WriteLine("****************************************");
+
+            for (int i = 0; i < kelimeler.anahtar.Length; i++)
+            {
+                Console.WriteLine(kelimeler.anahtar[i] + " : " + kelimeler.deger[i]);
+            }
+        }
+    }
+    
+}
